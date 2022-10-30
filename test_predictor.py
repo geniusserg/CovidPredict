61382cb4c4f955ad5f9d50ad8c5bfe79809a0a3b
sergey danilov <sddanilov_1@edu.hse.ru>
2022-10-30 17:29:33 +0300

    R2 on every param

--- test_predictor.py
+++ test_predictor.py
@@ -11,11 +11,9 @@ df["t_point"] = df["t_point"].apply(lambda x: int(x))
 df.set_index(["case", "t_point"], inplace=True)
 df.sort_index()
 
-test = [[36.6,1.9,41,72,19,10,9,0.024,18,0.0098,3,347,14],
-[36.6,1.9,41,72,19,10,9,0.024,18,0.0098,3,347,14],
-[36.6,1.9,41,72,19,10,9,0.024,18,0.0098,3,347,14]]
+test = df.loc[("GACAAcY")]
 
-model = CovidPatientStatePredictor(model_path="model1")
+model = CovidPatientStatePredictor(model_path="LSTM64xDense13")
 result = model.predict(test)
 
 print(f"Predictet patinet state: {result}")
