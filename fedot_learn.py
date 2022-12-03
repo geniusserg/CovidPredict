f298b8c4a835470d9e640be1b6a2ef0bddfc8e5c
Sergey Danilov <shyundaid@gmail.com>
2022-12-03 20:23:22 +0300

    Fedot experiements completed

---
+++ fedot_learn.py
@@ -0,0 +1,86 @@
+import os
+
+import numpy as np
+import pandas as pd
+import pickle 
+
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.impute import IterativeImputer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+# Plots
+import matplotlib.pyplot as plt
+
+# Prerocessing for FEDOT
+from fedot.core.data.data import InputData
+from fedot.core.repository.dataset_types import DataTypesEnum
+from fedot.api.main import Fedot
+from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
+
+# FEDOT 
+from fedot.core.pipelines.pipeline import Pipeline
+from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
+
+import logging
+logging.raiseExceptions = False
+
+df = None
+with open("covid_flow.pkl", "rb") as f:
+    df = pickle.load(f)
+df["case"] = df.index
+df["t_point"] = df["t_point"].apply(lambda x: x[2:])
+df["t_point"] = df["t_point"].apply(lambda x: int(x))
+df = df.set_index(["case", "t_point"])
+df = df.sort_values(["case", "t_point"])
+
+def prepare_data_with_dynamic_window(df_input, window=1, test_size=0.2):
+    X, y = [], []
+    df = df_input.copy()
+    df_grouped = df.groupby(["case"]).size()
+    df_idx = df_grouped[df_grouped>=window+1].index
+    for i in df_idx:
+        for j in range(len(df.loc[(i)])-window):
+            wind = df.loc[(i, j): (i, j+window-1)]
+            X.append(wind.iloc[:, :47])
+            y.append(df.loc[(i, j+window)].iloc[29:42])
+    X, y = np.array(X, dtype=np.float32), np.array(y, dtype=np.float32)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    return (X_train, y_train), (X_test, y_test)
+
+(X_train, y_train), (X_test, y_test) = prepare_data_with_dynamic_window(df, window=1)
+print("Test train shapes:", X_train.shape,
+X_test.shape)
+X_train = X_train.reshape(-1, 47)
+X_test = X_test.reshape(-1, 47)
+
+f = open("FEDOT_STATISTICS", "w")
+models = {}
+scores = {}
+dinam_fact_columns = df.columns[29:42]
+for param_idx, param_name in enumerate(dinam_fact_columns):
+    model = Fedot(problem='regression', timeout=10, n_jobs=-1)
+    try:
+        obtained_pipeline = model.fit(features=X_train, target=y_train[:, param_idx])
+        models[param_name] = (model, obtained_pipeline)
+        obtained_pipeline.save(f"models/fedot/fedot_regress_one_window_param_{param_idx}.json", datetime_in_path=False)
+        y_pred = models[param_name][0].predict(X_test)
+        mask = ~np.isnan(y_test[:, param_idx])
+        y_check = y_test[mask, param_idx]
+        y_pred = y_pred[mask]
+        r2_res = r2_score(y_check, y_pred, multioutput="raw_values")
+        mse = mean_squared_error(y_check, y_pred)
+        scores[param_name] = [r2_res[0], mse]
+        f.write(str([param_name, r2_res[0], mse]))    
+        print(f"SAVED {param_name}")
+    except:
+        print(f"FAILED {param_name}")
+        f.write(f"FAILED {param_name}")
+f.close()
+    
+
