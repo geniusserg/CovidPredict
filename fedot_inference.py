6466f00ce0bcf7c873ed5e2065d4d122ed69a1e3
Sergey Danilov <shyundaid@gmail.com>
2023-04-01 08:11:34 +0300

    Wrap python script to class

--- fedot_inference.py
+++ fedot_inference.py
@@ -22,29 +22,30 @@ from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
 from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
 
-models = os.listdir("fedot_pipelines")
-res = {}
-df_pred = pd.read_csv("input.csv")
-state_vector = df_pred.groupby("case").last()
+class FedotCovidPredict():
+    def __init__(self, models_directory:str = "fedot_pipelines"):
+        self.models = os.listdir("fedot_pipelines")
+        self.pipelines = {}
+        for model in self.models:
+            pipeline = Pipeline()
+            pipeline.load(f"fedot_pipelines/{model}/{model}.json")
+            self.pipelines[model] = pipeline
+    
+    def predict(self, df:pd.DataFrame):
+        state_vector = df.groupby("case").last() # use only last state vector
+        input_data = InputData(idx=state_vector.index, 
+                  features=state_vector, 
+                  data_type=DataTypesEnum.table,
+                  task=Task(TaskTypesEnum.regression))
 
-input_data = InputData(idx=state_vector.index, 
-          features=state_vector, 
-          data_type=DataTypesEnum.table,
-          task=Task(TaskTypesEnum.regression)
-         )
+        prediction_vector = {}
+        for model in models:
+            prediction_vector[model] = pipelines[model].predict(input_data).predict #sequentially run pipelines
 
-pipelines = {}
-for model in models:
-    pipeline = Pipeline()
-    pipeline.load(f"fedot_pipelines/{model}/{model}.json")
-    pipelines[model] = pipeline
-
-prediction_vector = {}
-for model in models:
-    prediction_vector[model] = pipelines[model].predict(input_data).predict
-
-# Categorical features
-result = pd.DataFrame(prediction_vector)
-result.loc[:, "снижение_сознания_dinam_fact"] = result["снижение_сознания_dinam_fact"].apply(lambda x: int(x))
-result.loc[:, "Cтепень тяжести по КТ_dinam_fact"] = result["Cтепень тяжести по КТ_dinam_fact"].apply(lambda x: int(x))
-result.to_csv("output.csv") 
+        # Round categorical features 
+        result = pd.DataFrame(prediction_vector)
+        result.loc[:, "снижение_сознания_dinam_fact"] = result["снижение_сознания_dinam_fact"].apply(lambda x: int(x))
+        result.loc[:, "Cтепень тяжести по КТ_dinam_fact"] = result["Cтепень тяжести по КТ_dinam_fact"].apply(lambda x: int(x))
+        return result 
+        
+        
