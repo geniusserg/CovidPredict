300482591bec68aa563101232bf1c02890b63ffd
geniusserg <50704370+geniusserg@users.noreply.github.com>
2023-04-03 10:46:16 +0000

    fix fedot_inference - typos. Notebook fixes

--- fedot_inference.py
+++ fedot_inference.py
@@ -10,43 +10,36 @@
 #####
 
 import os
-
-import numpy as np
 import pandas as pd
-
 
 from fedot.core.data.data import InputData
 from fedot.core.repository.dataset_types import DataTypesEnum
-from fedot.api.main import Fedot
-from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
+from fedot.core.repository.tasks import Task, TaskTypesEnum
 from fedot.core.pipelines.pipeline import Pipeline
-from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
 
 class FedotCovidPredict():
-    def __init__(self, models_directory:str = "fedot_pipelines"):
-        self.models = os.listdir(model_directory)
+    def __init__(self, models_directory:str = "fedot_pipelines_april"):
+        self.models = os.listdir(models_directory)
         self.pipelines = {} # here stored fitted Pipelines
         for model in self.models:
             pipeline = Pipeline()
-            pipeline.load(f"{model_directory}/{model}/{model}.json")
+            pipeline.load(f"{models_directory}/{model}/{model}.json")
             self.pipelines[model] = pipeline
     
     def predict(self, df:pd.DataFrame):
         state_vector = df.groupby("case").last() #  Our models use only one vector
         input_data = InputData(idx=state_vector.index,  # transform to InputData
-                  features=state_vector, 
+                  features=state_vector,
                   data_type=DataTypesEnum.table,
                   task=Task(TaskTypesEnum.regression))
 
         #sequentially run pipelines on all parameters
-        prediction_vector = {} 
-        for model in models:
-            prediction_vector[model] = pipelines[model].predict(input_data).predict 
+        prediction_vector = {}
+        for model in self.models:
+            prediction_vector[model] = self.pipelines[model].predict(input_data).predict
 
-        # Round values of categorical features 
+        # Round values of categorical features
         result = pd.DataFrame(prediction_vector)
         result.loc[:, "снижение_сознания_dinam_fact"] = result["снижение_сознания_dinam_fact"].apply(lambda x: int(x))
         result.loc[:, "Cтепень тяжести по КТ_dinam_fact"] = result["Cтепень тяжести по КТ_dinam_fact"].apply(lambda x: int(x))
-        return result 
-        
-        
+        return result
